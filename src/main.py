8bf31a3e1b64985d02e59f1e81c4b433921fe4d4
amir-bashiri <amirbashiri.v@gmail.com>
2022-02-06 21:38:30 +0000

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,35 @@
 import telebot
 import os
+import sys
+import time
+from loguru import logger
 
-bot = telebot.TeleBot(os.environ['teletoken'], parse_mode=None)
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, 'Howdy, how are you doing?')
-    
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-    
-bot.infinity_polling()
+
+class Telechat:
+    def __init__(self):
+        # self.token = os.environ['teletoken']
+        self.bot = telebot.TeleBot(os.environ['teletoken'], parse_mode=None)
+        self.start()
+        logger.info("Starting bot")
+        
+
+    def start(self):
+        logger.info("some info")
+        
+        @self.bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            self.bot.reply_to(message, 'Howdy, how are you doing?')
+
+
+        @self.bot.message_handler(func=lambda m: True)
+        def echo_all(message):
+            self.bot.reply_to(message, message.text)
+        
+        self.bot.infinity_polling()
+
+    # def run(self):
+    #     self.bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    chat = Telechat()
